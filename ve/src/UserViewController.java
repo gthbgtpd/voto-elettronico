6f7191335e4f3ea754e9f2eaeb0d232a78e8ee15
Luca Marazzi <marazzi.luca99@gmail.com>
2022-03-04 20:04:21 +0100

    Aggiunta scheletro di controller della vista utente

---
+++ ve/src/UserViewController.java
@@ -0,0 +1,82 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.Pane;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ListView<?> ListViewVotableOrdered;
+
+    @FXML
+    private ListView<?> ListViewVotableUnordered;
+
+    @FXML
+    private MenuButton ScegliSessioneAperta;
+
+    @FXML
+    private ListView<?> SelezionaCandidatiPartito;
+
+    @FXML
+    private MenuButton SelezionaPartito;
+
+    @FXML
+    private MenuButton SelezionaPreferenza;
+
+    @FXML
+    private Pane categoricoPane;
+
+    @FXML
+    private Pane categoricoPreferenzaPane;
+
+    @FXML
+    private Pane openVoteSessionPane;
+
+    @FXML
+    private Pane ordinalePane;
+
+    @FXML
+    private CheckBox referendumContro;
+
+    @FXML
+    private Pane referendumPane;
+
+    @FXML
+    private CheckBox referendumPro;
+
+    @FXML
+    private Button votaCategoricoPreferenzaButton;
+
+    @FXML
+    private Button votoCategoricoButton;
+
+    @FXML
+    private Button votoOrdinaleButton;
+
+    @FXML
+    private Button votoReferendumButton;
+
+    @FXML
+    void handleVotoCategorico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleVotoCategoricoPreferenza(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleVotoOrdinale(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleVotoReferendum(ActionEvent event) {
+
+    }
+
+}
