deb539c3bd527312149ecc2d2d4a21a1e4144b7d
Luca Marazzi <marazzi.luca99@gmail.com>
2022-03-06 11:55:06 +0100

    Aggiunta metodo getVotingSession per interagire con il DB e restituire una sessione di voto
    Potrebbe essere re-implementato modificando alcuni metodi per renderli più ri-usabili e meno accoppiati

--- ve/src/UserViewDao.java
+++ ve/src/UserViewDao.java
@@ -4,15 +4,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import Model.Candidato;
+import Model.Gruppo;
+import Model.SessioneVoto;
+import Model.SessioneVotoFactory;
+import Model.Votable;
 
 public class UserViewDao {
-	
+
 	private static Connection getConnection() throws SQLException {
 	  	final String url = "jdbc:postgresql://localhost/votoelettronico?user=root&password=rootroot";
 		Connection conn = null;
@@ -23,7 +33,7 @@ public class UserViewDao {
 		}
 		return conn;
 	}
-	
+
 	public static void putVote(Votable candidato, Integer value) throws SQLException {
 		Connection conn = getConnection();
 		int id=0;
@@ -33,8 +43,8 @@ public class UserViewDao {
         while ( res.next() ) {
         	id = res.getInt(1);
         }
-		
-		
+
+
 		PreparedStatement query1 = conn.prepareStatement("update voto.vote set preferenza=? where id=?");
         query1.setInt(1, value);
         query1.setInt(2, id);
@@ -42,7 +52,7 @@ public class UserViewDao {
 		int r = query1.executeUpdate();
         return;
 	}
-	
+
 	public static void getSessions(MenuButton scegliSessione) throws SQLException {
 		scegliSessione.getItems().clear();
 		List<String> l = new ArrayList<>();
@@ -53,7 +63,7 @@ public class UserViewDao {
 		while(r.next()) {
 			l.add(r.getString("name"));
 		}
-		
+
 		for (String i:l) {
     		MenuItem session = new MenuItem(i);
         	scegliSessione.getItems().add(session);
@@ -96,7 +106,7 @@ public class UserViewDao {
 		while(r1.next()) {
 			idp = r1.getInt(1);
 		}
-		
+
 		ObservableList<String> data = FXCollections.observableArrayList();
 		String sql = "select c.name from voto.candidates as c join voto.partytable as p on p.idpartymember=c.id where p.idparty=?";
 		PreparedStatement stmt = conn.prepareStatement(sql);
@@ -120,5 +130,84 @@ public class UserViewDao {
 		}
 		return data;
 	}
+	
+	public static SessioneVoto getVotingSession(String nameSession) throws SQLException {
+		Connection con = getConnection();
+		SessioneVotoFactory sf = new SessioneVotoFactory();
+		String firstQuery = "SELECT * FROM voto.session AS s WHERE s.name = ?";
+		PreparedStatement s = con.prepareStatement(firstQuery);
+		s.setString(1, nameSession);
+		ResultSet res = s.executeQuery();
+		int idSessione = 1; // valore di default nel caso il nome sia scorretto 
+		while(res.next()) {
+			idSessione = res.getInt("id");
+			String modeVote = res.getString("modevote");
+			boolean isOpen = res.getBoolean("isopen");
+			String modeWin = res.getString("modewin");
+			boolean scrutinyPhase = res.getBoolean("fasescrutinio");
+			Date beginningDate = res.getDate("dataapertura");
+			Date endingDate = res.getDate("datachiusura");
+			sf.setId(idSessione);
+			sf.setDefinizioneVincitore(modeWin);
+			sf.setBeginningDate(beginningDate);
+			sf.setEndingDate(endingDate);
+			sf.setScrutinyPhase(scrutinyPhase);
+			sf.setOpen(isOpen);
+			sf.setModalitaVotoName(modeVote);
+		}
+		s.close();
+		res.close();
+		String secondQuery = "SELECT * FROM voto.candidates as c WHERE c.idsession = ?";
+		s = con.prepareStatement(secondQuery);
+		s.setInt(1, idSessione);
+		res = s.executeQuery();
+		Set<Votable> candidates = new HashSet<>();
+		while(res.next()) {
+			int idCandidate = res.getInt("id");
+			String name = res.getString("name");
+			boolean isParty = res.getBoolean("isparty");
+			Votable v;
+			if (isParty) {
+				// può essere un metodo a parte per aumentare il riuso 
+				Set<Candidato> candidatesInParty = new HashSet<>();
+				String thirdQuery = "SELECT * FROM voto.partytable as pt JOIN voto.candidates as c ON pt.idpartymember = c.id WHERE pt.idparty = ?";
+				s = con.prepareStatement(thirdQuery);
+				s.setInt(1, idCandidate);
+				ResultSet resCandidates = s.executeQuery();
+				s.close();
+				while(resCandidates.next()) {
+					int idCandidateParty = resCandidates.getInt("id");
+					String nameCandidateParty = resCandidates.getString("name");
+					candidatesInParty.add(new Candidato(idCandidateParty, nameCandidateParty));
+				}
+				resCandidates.close();
+				// fine metodo in comune
+				v = new Gruppo(idCandidate, name, candidatesInParty);
+			} else {
+				v = new Candidato(idCandidate, name);
+			}
+			candidates.add(v);
+		}
+		s.close();
+		res.close();
+		Map<Votable, Integer> votes = new HashMap<>();
+		for (Votable v : candidates) {
+			String fourthQuery = "SELECT * FROM voto.vote as v WHERE v.idsession = ? AND v.idcandidates = ?";
+			s = con.prepareStatement(fourthQuery);
+			s.setInt(1, idSessione);
+			s.setInt(2, v.getId());
+			res = s.executeQuery();
+			while (res.next()) {
+				int voti = res.getInt("preferenza");
+				votes.put(v, voti);
+			}
+			res.close();
+			s.close();
+		}
+		sf.setVotes(votes);
+		con.close();
+		return sf.getVotingSession();
+	}
 
 }
+
