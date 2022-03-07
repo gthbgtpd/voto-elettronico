5b6425b2790dcb225f5e05082a2b2cf7ec671463
Luca Marazzi <marazzi.luca99@gmail.com>
2022-03-07 19:21:05 +0100

    Aggiunta di un limite massimo per le preferenze in una votazione ordinale

--- ve/src/Model/ConcreteStrategyOrdinale.java
+++ ve/src/Model/ConcreteStrategyOrdinale.java
@@ -1,21 +1,25 @@
 package Model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ConcreteStrategyOrdinale {
+public class ConcreteStrategyOrdinale implements ModalitaVotoStrategy  {
 
+	private final int NUM_MAX_PREF = 100; // numero massimo di preferenze esprimibili in un voto ordinale
+	
 	public ConcreteStrategyOrdinale() {}
 	
 	public Map<Votable, Integer> voto (List<Votable> preferenze, Map<Votable, Integer> storicoPreferenze) {
-		int punteggio = preferenze.size();
-		for (Votable candidato: preferenze) {
+		int punteggio = NUM_MAX_PREF;
+		Map<Votable, Integer> preferenzeModificate = new HashMap<>();
+		for (Votable candidato : preferenze) {
 			int punteggioPrecedente = 0;
 			if (storicoPreferenze.containsKey(candidato)) punteggioPrecedente = storicoPreferenze.get(candidato);
-			storicoPreferenze.put(candidato, punteggio + punteggioPrecedente);
+			preferenzeModificate.put(candidato, punteggio + punteggioPrecedente);
 			punteggio--;
 		}
-		return storicoPreferenze; // tendenzialmente non sarebbe nemmeno necessario restituirlo perché la modifica avviene inplace
+		return preferenzeModificate;
 	}
 	
 }
