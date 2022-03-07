483b1d156f8222de2c41b9bf162c55f3d9201065
Luca Marazzi <marazzi.luca99@gmail.com>
2022-03-07 19:21:54 +0100

    Update ConcreteStrategyCategorico.java

--- ve/src/Model/ConcreteStrategyCategorico.java
+++ ve/src/Model/ConcreteStrategyCategorico.java
@@ -1,15 +1,19 @@
 package Model;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ConcreteStrategyCategorico {
+public class ConcreteStrategyCategorico implements ModalitaVotoStrategy {
 
 	public ConcreteStrategyCategorico() {}
 	
-	public Map<Votable, Integer> voto (Votable preferenza, Map<Votable, Integer> storicoPreferenze) {
+	public Map<Votable, Integer> voto (List<Votable> preferenze, Map<Votable, Integer> storicoPreferenze) {
+		Votable preferenza = preferenze.get(0);
 		int punteggioPrecedente = 0;
+		Map<Votable, Integer> preferenzeModificate = new HashMap<>();
 		if (storicoPreferenze.containsKey(preferenza)) punteggioPrecedente = storicoPreferenze.get(preferenza);
-		storicoPreferenze.put(preferenza, punteggioPrecedente + 1);
-		return storicoPreferenze; // tendenzialmente non sarebbe nemmeno necessario restituirlo perché la modifica avviene inplace
+		preferenzeModificate.put(preferenza, punteggioPrecedente + 1);
+		return preferenzeModificate; // tendenzialmente non sarebbe nemmeno necessario restituirlo perché la modifica avviene inplace
 	}
 }
