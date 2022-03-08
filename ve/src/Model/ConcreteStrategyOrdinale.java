8efef1b8a765b66fe9e0e8ad73b934eecc0fea5e
Luca Marazzi <marazzi.luca99@gmail.com>
2022-03-08 12:56:15 +0100

    Update ConcreteStrategyOrdinale.java

--- ve/src/Model/ConcreteStrategyOrdinale.java
+++ ve/src/Model/ConcreteStrategyOrdinale.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class ConcreteStrategyOrdinale implements ModalitaVotoStrategy  {
-
-	private final int NUM_MAX_PREF = 100; // numero massimo di preferenze esprimibili in un voto ordinale
 	
 	public ConcreteStrategyOrdinale() {}
 	
 	public Map<Votable, Integer> voto (List<Votable> preferenze, Map<Votable, Integer> storicoPreferenze) {
-		int punteggio = NUM_MAX_PREF;
+		int punteggio = storicoPreferenze.keySet().size();
 		Map<Votable, Integer> preferenzeModificate = new HashMap<>();
 		for (Votable candidato : preferenze) {
 			int punteggioPrecedente = 0;
