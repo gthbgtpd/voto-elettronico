75fedd74c7b11ce409f63b5ac438efee2a3b206f
tsu <72899402+gthbgtpd@users.noreply.github.com>
2022-06-02 19:06:52 +0200

    Add files via upload
    Aggiunta la parte del login (gui, controller  e la classe MD5Util che cripta le password)

--- ve/src/Main.java
+++ ve/src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 	
 	@Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("AdminView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         primaryStage.setTitle("voto elettronico");
         primaryStage.setScene(new Scene(root, 800, 600));
